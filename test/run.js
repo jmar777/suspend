ba80c0a47ee72a5dda9a790e90f60ed3637f7ae4
Jeremy Martin <jmar777@gmail.com>
2013-11-10 20:03:36 -0500

    refactor test/run.js for brevity

--- test/run.js
+++ test/run.js
@@ -1,90 +1,39 @@
-var fs = require("fs");
-var semver = require("semver");
-var spawn = require("child_process").spawn;
-var regenerator = require("regenerator");
+var fs = require('fs'),
+	semver = require('semver'),
+	regenerator = require('regenerator'),
+	spawn = require('child_process').spawn,
+	mochaArgs = ['--reporter', 'list'];
 
-function convert(es6File, es5File, callback) {
-	fs.readFile(es6File, "utf-8", function(err, es6) {
-		if (err) {
-			return callback(err);
-		}
+// get our test files
+var testFiles = fs.readdirSync(__dirname).filter(function(fileName) {
+	return !/(?:^run|\.es5)\.js$/.test(fileName);
+}).map(function(fileName) {
+	return './test/' + fileName;
+});
 
-		fs.writeFile(es5File, regenerator(es6, {
-			includeRuntime: true
-		}), callback);
+// check if we're running with --harmony-generators or regenerator
+if (semver.gte(process.version, '0.11.2')) {
+	console.log('Running tests with --harmony-generators...');
+	mochaArgs = mochaArgs.concat(['--harmony-generators']).concat(testFiles);
+} else {
+	console.log('Running tests with regenerator...')
+	// regenerate generator code to *.es5.js files
+	testFiles.forEach(function(fileName) {
+		console.log('Regenerating file:', fileName);
+		var fileContents = fs.readFileSync(fileName, 'utf-8'),
+			es5Name = fileName.replace(/\.js$/, '.es5.js'),
+			regenerated = regenerator(fileContents, { includeRuntime: true });
+		fs.writeFileSync(es5Name, regenerated);
+		mochaArgs.push(es5Name);
 	});
 }
 
-var queue = [];
-function enqueue(cmd, args) {
-	queue.push({
-		cmd: cmd,
-		args: args || []
-	});
-}
-
-function flush() {
-	var entry = queue.shift();
-	if (entry) {
-		var cmd = entry.cmd;
-		if (typeof cmd === "function") {
-			if (cmd.length > 1) {
-				// If the command accepts a parameter, let it call the
-				// asyncCallback itself.
-				cmd.apply(null, entry.args.concat(asyncCallback));
-			} else {
-				cmd.apply(null, entry.args);
-				asyncCallback();
-			}
-		} else {
-			spawn(cmd, entry.args, {
-				stdio: "inherit"
-			}).on("exit", asyncCallback);
-		}
-	}
-}
-
-function asyncCallback(err) {
+// run the tests with mocha
+spawn('./node_modules/mocha/bin/mocha', mochaArgs, {
+	stdio: 'inherit'
+}).on('exit', function(err) {
 	if (err) {
-		console.error("process exited abnormally:", err);
-		process.exit(typeof err === "number" ? err : -1);
-	} else {
-		process.nextTick(flush);
+		console.error('process exited abnormally:', err);
+		process.exit(typeof err === 'number' ? err : -1);
 	}
-}
-
-enqueue(convert, [
-	"./test/promises.js",
-	"./test/promises.es5.js"
-]);
-
-enqueue(convert, [
-	"./test/resume.js",
-	"./test/resume.es5.js"
-]);
-
-enqueue(convert, [
-	"./test/snafus.js",
-	"./test/snafus.es5.js"
-]);
-
-if (semver.gte(process.version, "0.11.2")) {
-	enqueue(console.log.bind(console), ["Running tests with --harmony-generators..."]);
-	enqueue("node", [
-		"node_modules/mocha/bin/mocha",
-		"--harmony-generators",
-		"--reporter", "list",
-		"./test/promises.js",
-		"./test/resume.js",
-		"./test/snafus.js"
-	]);
-}
-
-enqueue(console.log.bind(console), ["Running tests with regenerator..."]);
-enqueue("node", [
-	"node_modules/mocha/bin/mocha",
-	"--reporter", "list",
-	"./test/*.es5.js"
-]);
-
-flush();
+});
